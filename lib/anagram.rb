44cc427e57b1d63436d54352c648e52dccc77a3d
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2023-01-18 12:01:53 +1100

    Done with assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize (word)
+        @word = word
+    end
+
+    def match array
+       array.filter do |element|
+         word.split("").sort == element.split("").sort
+       end
+    end
+end
+
+# word = "bananas"
+
+# pp word.split("").sort
